9caa8cfae8f6b7b8ba80350cf3dc3096d2d6d970
Alberto_ds <alber.disegni@gmail.com>
2026-06-16 16:46:36 +0200

    add split_nodes_delimiter for inline markdown parsing

---
+++ src/inline_markdown.py
@@ -0,0 +1,23 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    nodes_list = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            nodes_list.append(old_node)
+        else:
+            split_text = old_node.text.split(delimiter)
+            if len(split_text) % 2 == 0:
+                raise Exception("invalid Markdown syntax")
+            new_nodes = []
+            for i in range(len(split_text)):
+                if split_text[i] == "":
+                    continue
+                if i % 2 == 0:
+                    new_node = TextNode(split_text[i], TextType.TEXT)
+                    new_nodes.append(new_node)
+                else:
+                    new_node = TextNode(split_text[i], text_type)
+                    new_nodes.append(new_node)
+            nodes_list.extend(new_nodes)
+    return nodes_list
