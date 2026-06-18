92c2cc4ee8149886b333a06de13687ad18acf4bf
Alberto_ds <alber.disegni@gmail.com>
2026-06-18 17:40:09 +0200

    add BlockType enum and block_to_block_type function

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -46,6 +46,35 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+        
+        
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading(self):
+        result = block_to_block_type("# this is an h1")
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_code(self):
+        result = block_to_block_type("```\n this is a code \n```")
+        self.assertEqual(result, BlockType.CODE)
+
+    def test_quote(self):
+        result = block_to_block_type("> this is a quote")
+        self.assertEqual(result, BlockType.QUOTE)
+
+    def test_ul(self):
+        result = block_to_block_type("- this is an ul -")
+        self.assertEqual(result, BlockType.UNORDERED_LIST)
+        
+    def test_ol(self):
+        result = block_to_block_type("1. this is an ol\n2. this is an ol\n3. this is an ol")
+        self.assertEqual(result, BlockType.ORDERED_LIST)
+
+    def test_p(self):
+        result = block_to_block_type("this is a p")
+        self.assertEqual(result, BlockType.PARAGRAPH)
+    
+    
+
 
 
 if __name__ == "__main__":
